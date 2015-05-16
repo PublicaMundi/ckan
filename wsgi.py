b0a53c1e84610dd6ff46a53fd88be332875f40b4
drmalex07 <alexakis@imis.athena-innovation.gr>
2015-05-16 19:09:08 +0300

    Modified wsgi.py to expect a neutral config file
    Removed reference to `development.ini`. The same WSGI loader script
    can be used with an arbitrary config INI file.

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,22 @@
 import logging, logging.config
 import os
 
+# Expect `config.ini` to be present as the main configuration file. 
+# Note: Make it a symbolic link to either development.ini or production.ini.
 here = os.path.dirname(os.path.realpath(__file__))
-ini_file = os.path.join(here, 'development.ini')
+config_file = os.path.join(here, 'config.ini')
 
 # Activate enviroment
 activate_this = os.path.realpath(os.path.join(here, '../..', 'bin/activate_this.py'))
 execfile(activate_this, dict(__file__=activate_this))
 
 # Setup loggers
-logging.config.fileConfig(ini_file)
+logging.config.fileConfig(config_file)
 
 from paste.deploy import loadapp
 
 # Load WSGI application
-application = loadapp('config:%s' %(ini_file));
+application = loadapp('config:%s' %(config_file));
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
